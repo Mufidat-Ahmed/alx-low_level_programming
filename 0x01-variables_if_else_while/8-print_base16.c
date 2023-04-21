627022e82f387e34d209fd7da95bffb694a9c7fc
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-21 08:18:29 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char low;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (low = 'a'; low <= 'f'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
