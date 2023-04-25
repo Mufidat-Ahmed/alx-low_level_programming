7319ea7fd01be47cde0ff5efcf810180ab6b4e2e
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-25 14:01:34 -0700

    Naturals

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - list natural numbers
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i > 1024; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			j += i;
+
+	}
+	printf("%d\n", j);
+
+	return (0);
+}
