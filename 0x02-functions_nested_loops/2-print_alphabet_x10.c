18f1e636100716b10a358a5a51c48f5b457c6192
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-25 07:05:48 -0700

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet in lowercaser 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char alpha;
+
+	while (i < 11)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar("\n");
+		i++;
+	}
+}
