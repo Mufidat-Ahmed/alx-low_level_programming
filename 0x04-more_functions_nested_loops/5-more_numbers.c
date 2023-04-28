151fa4da9a5f607f5b753313a2c55946675ef00d
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-28 05:42:31 -0700

    More number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the number from 0 -14
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 11)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+		i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
