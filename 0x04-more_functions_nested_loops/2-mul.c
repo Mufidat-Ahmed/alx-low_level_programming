23da67f3fcbd70e739031e52b66483fac49f3d1d
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-28 04:50:42 -0700

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multipy two digits
+ * @a: int to be multiplied
+ * @b: int to be multiplied
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	int mul = (a * b);
+
+	return (mul);
+}
