9d8f372687eef8d63b406c35f8fada54d6d3007c
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-28 05:15:25 -0700

    Print-numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print form 0 - 9 followed by newline
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
