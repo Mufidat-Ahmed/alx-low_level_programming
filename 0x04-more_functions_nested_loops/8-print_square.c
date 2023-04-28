cdfbe2e37e678e524cc6cdc69a41cf5b55d8ee04
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-04-28 09:41:25 -0700

    Hash

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,9 @@ void print_square(int size)
 	{
 		int i, n;
 
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (n = 0; n <= size; n++)
+			for (n = 0; n < size; n++)
 			{
 				_putchar('#');
 			}
