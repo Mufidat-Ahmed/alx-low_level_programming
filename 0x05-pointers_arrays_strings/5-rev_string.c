9607df68fcf964558b2900bd7e26836b7eefa700
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-02 07:05:17 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,16 @@
 
 void rev_string(char *s)
 {
-	char rev = s[0];
-	int j = 0, i;
+	char str = s[0];
+	int j = -1, i, len;
 
-	while (s[j] != '\0')
-		j++;
+	while (s[j] != 0);
+	j++;
 		for (i = 0; i < j; i++)
 		{
-			j--;
-			rev = s[i];
+			str = s[i];
 			s[i] = s[j];
-			s[j] = rev;
+			s[j] = str;
+			j--;
 		}
 }
