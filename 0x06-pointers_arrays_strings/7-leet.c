91edcfce80c5abf2a29a8f8129dc1f14b1ae6c07
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-04 12:30:12 -0700

    Mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: charater to be evaluated
+ * Return: n
+ */
+
+char *leet(char *n)
+{
+	int i, m;
+	char k1[] = "aAeEoOlLtT";
+	char k2[] = "4433007711";
+
+	for (i = 0; n[i] != '\n'; i++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			if (n[i] == k1[m])
+			{
+				n[i] == k2[m];
+			}
+		}
+	}
+	return (n);
+}
