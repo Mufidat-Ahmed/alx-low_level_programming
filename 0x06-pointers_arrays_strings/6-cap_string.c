e97b34f8d1029e183f50575b58e041321d224165
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-04 11:46:23 -0700

    String capitalization

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalises all words of a string
+ * Return: n
+ * @n: string to br scanned
+ */
+
+char *cap_string(char *n)
+{
+	int index = 0;
+
+	while (n[index])
+	{
+		while (n[index] >= 97 && n[index] <= 122)
+			index++;
+		if (n[index - 1] == ' ' ||
+				n[index - 1] == '\t' ||
+				n[index - 1] == '\n' ||
+				n[index - 1] == ',' ||
+				n[index - 1] == ';' ||
+				n[index - 1] == '.' ||
+				n[index - 1] == '!' ||
+				n[index - 1] == '?' ||
+				n[index - 1] == '"' ||
+				n[index - 1] == '(' ||
+				n[index - 1] == ')' ||
+				n[index - 1] == '{' ||
+				n[index - 1] == '}' ||
+				index == 0)
+			n[index] -= 32;
+		index++;
+	}
+	return (n);
+}
