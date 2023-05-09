131d300fbfb5bb6b47e922f278611cbf941b0bba
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-08 23:40:42 -0700

    Strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,7 +9,7 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (;*haystack != '\0'; haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
 		while (*haystack == *needle && *needle != '\0')
 		{
