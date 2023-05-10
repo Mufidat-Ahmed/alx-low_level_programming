2c90b464aa87920682a577eb23e06ca3abd42a3b
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-10 12:59:55 -0700

    1

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,31 +8,17 @@
  * Return: Square root of n
  */
 
-int new_sqrt_recursion(int n, int m);
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-}
-/**
- * new_sqrt_recursion - prints the square root of a number
- * @m: int thats repeated
- * @n: natural number to be used
- * Return: square root of n
- */
+	int m;
 
-int new_sqrt_recursion(int n, int m)
-{
 	m = n * n;
 
-	if (m > n)
+	if (n <= 0)
+		return (-1);
+	else if (m >= n)
 	{
 		return (-1);
 	}
-	else if (m == n)
-	{
 		return (m);
-	}
-	else
-	return (_sqrt_recursion(m));
 }
