667c8590d60279823f22d52355a1c5f365692ff8
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-11 13:17:44 -0700

    Inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,20 +6,23 @@
  * @m: iterator
  * Return: 1 or 0
  */
+
 int prime_number(int n, int m);
 int is_prime_number(int n)
 {
 	return (prime_number(n, n - 1));
 }
+
 /**
  * prime_number - gives prime number
  * @n: integer to containg prime number
  * @m: iterator
  * Return: 1 or 0
  */
+
 int prime_number(int n, int m)
 {
-	if (n < 1)
+	if (n <= 1)
 		return (0);
 	else if (m == 1)
 		return (1);
