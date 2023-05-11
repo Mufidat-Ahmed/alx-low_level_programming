7a3a63be6c4ed6ab2fb0c24ef61b9620eb1fa2b4
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-11 11:02:16 -0700

    Prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,13 +15,15 @@ int is_prime_number(int n)
 }
 /**
  * prime_number - gives prime number
+ * @n: integer to containg prime number
  * @m: iterator
+ * Return: 1 or 0
  */
 int prime_number(int n, int m)
 {
-	if (m == 1)
-		return (1);
 	if (n % m == 0 && m > 0);
 	return (0);
+	if (m == 1)
+		return (1);
 	return (prime_number(n, m - 1));
 }
