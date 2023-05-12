8701160db1497900560ecb4e5e404785c6ff32e3
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-05-12 05:23:42 -0700

    My name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name followed by a new name
+ * @argc: number of arguments accepted by argv
+ * @argv: Array accepting arguments
+ * Return: 0 always success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
