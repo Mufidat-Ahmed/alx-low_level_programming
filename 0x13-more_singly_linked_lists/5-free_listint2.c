4c43e2df859f6b4db47178b5f9656f9b6db49b7b
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-06-06 00:12:05 -0700

    free listint

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees listsint_t
+ * @head: points to the list to be freed
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		new = (*head)->next;
+		free(*head);
+		*head = new;
+	}
+	*head = NULL;
+}
